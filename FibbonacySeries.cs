8e1e06349f56f3e0876415962901d48aaaf86fab
alcodepro-dev <88611298+alcodepro-dev@users.noreply.github.com>
2024-07-04 23:48:56 +0530

    Add files via upload

---
+++ FibbonacySeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_BasicProgram
+{
+    internal class FibbonacySeries
+    {
+        public void fibbonacy()
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+
+            for (int i = 0; i < 10; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = c;
+                b = c;
+                c = a;
+            }
+           
+        }
+
+    }
+}
